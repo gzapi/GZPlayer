78f8f2d0d99f8525d89deb39fd0f6435ff38a860
Bruno Cezar <dev@gzapi.gz2.com.br>
2025-07-13 09:29:06 -0300

    Ajustes

--- src/environments/environment.ts
+++
@@ -1,9 +0,0 @@
-export const environment = {
-    production: false,
-    DB_NAME: "gz_play",
-    DB_USER: "gz_play",
-    DB_PASS: "3623br@B",
-    DB_HOST: "pg.gedaim.com",
-    DB_PORT: 5432,
-    API_URL: "http://localhost:5000"
-};
